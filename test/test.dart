9807c421d8ed355d8a2bd0606dd63db08e7b3b96
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-26 16:00:15 +0530

    Added test for Barrett Reduction Computation
    - Added a test for Barrett Reduction, for the computation part
    
    - This computes the reduction from a number

--- test/test.dart
+++ test/test.dart
@@ -6,36 +6,68 @@ import 'package:kyber/kyber.dart';
 
 void main() async {
   group('Montgomery Reduction', () {
-    test('Positive', () async {
+    test('Positive', () {
       var file = File('test/montgomery_reduce_positive.json');
-      var content = await file.readAsString();
+      var content = file.readAsStringSync();
       var values = (jsonDecode(content)['values'] as List<dynamic>)
           .map((e) => e as int)
           .toList();
-      bool failure = false;
+      var failure = false;
+      var fails = <int>[];
       for (int i = 0; i < values.length; i++) {
         if (values[i] != KyberFunctions.montgomeryReduce(i)) {
-          _log('FAIL :: $i', _ConsoleColors.red);
           failure = true;
+          fails.add(i);
         }
+      }
+      if (failure) {
+        _log('FAILURES :: $fails', _ConsoleColors.red);
       }
       expect(failure, false);
     });
-    test('Negative', () async {
+    test('Negative', () {
       var file = File('test/montgomery_reduce_negative.json');
-      var content = await file.readAsString();
+      var content = file.readAsStringSync();
       var values = (jsonDecode(content)['values'] as List<dynamic>)
           .map((e) => e as int)
           .toList();
       bool failure = false;
+      var fails = <int>[];
       for (int i = -values.length; i > 0; i++) {
         if (values[values.length + i] != KyberFunctions.montgomeryReduce(i)) {
-          _log('FAIL :: $i', _ConsoleColors.red);
           failure = true;
+          fails.add(i);
         }
+      }
+      if (failure) {
+        _log('FAILURES :: $fails', _ConsoleColors.red);
       }
       expect(failure, false);
     });
+  });
+
+  group('Barrett Reduction', () {
+    test('Computation', () {
+      var file = File('test/barrett_computation.json');
+      var contents = file.readAsStringSync();
+      var values = (jsonDecode(contents)['values'] as List<dynamic>)
+          .map((e) => e as int)
+          .toList();
+      var failure = false;
+      var fails = <int>[];
+      for (int i = 0; i < 100000; i++) {
+        if (values[i] != NTT.computeBarret(i)) {
+          failure = true;
+          fails.add(i);
+        }
+      }
+      if (failure) {
+        _log('FAILURES :: $fails', _ConsoleColors.red);
+      }
+      expect(failure, false);
+    });
+
+    test('Reduce Polynomial', () {});
   });
 }
 
@@ -45,9 +77,10 @@ void _log(String text, _ConsoleColors? color) {
       _cColors[_ConsoleColors.normal]!);
 }
 
-enum _ConsoleColors { red, normal }
+enum _ConsoleColors { red, green, normal }
 
 Map<_ConsoleColors, String> _cColors = {
   _ConsoleColors.red: '\x1b[0;31m',
+  _ConsoleColors.green: '\x1b[0;32m',
   _ConsoleColors.normal: '\x1b[0m',
 };
